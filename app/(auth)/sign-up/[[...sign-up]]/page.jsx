59fb266cced291b323fca249180832a7b3a1ff18
wordpressperson <52249958+wordpressperson@users.noreply.github.com>
2026-04-03 03:44:06 +0100

    Update page.jsx

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -3,7 +3,3 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return <SignUp afterSignUpUrl="/dashboard" />;
 }
-
-export default function Page() {
-  return <SignUp />;
-}
