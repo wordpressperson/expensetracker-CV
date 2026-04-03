eeaed427165c9d52b02427e6835397d284a849a8
wordpressperson <52249958+wordpressperson@users.noreply.github.com>
2026-04-03 03:42:38 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes:['/']
+  publicRoutes: ["/", "/sign-in(.*)", "/sign-up(.*)"],
 });
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
